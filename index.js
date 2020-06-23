3a6c917cfc47a6df8c43b8e43197cf13e1ec95c6
Samuel Skeen <sskeen@sskeendevelopment.com>
2020-06-23 08:17:01 -0600

    update example to use the aes-256-gcm algorithm instead

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const { Transform } = require('stream');
 
 // A simple transform stream that appends the IV to the file so that it can be used when encrypting the file
+// When dealing with data not being written to files, this can be done with a Buffer.concat call instead, which might be simpler for your use case
 class AppendIv extends Transform {
     constructor(iv) {
         super();
@@ -49,51 +50,56 @@ readStream.on('error', err => {
 });
 
 // When it completes, the result is as if it was saved in the system
-encryptedWriteStream.on('close', () => {
+encryptedWriteStream.on('close', async () => {
+    // When using aes-256-gcm algorithm, it generates an auth tag required to authenticate the decipher algorithm in order for it to tell if the decrypted data should be trusted
+    // Previously, while using aes-256-cbc, the auth tag was not generated or needed, but otherwise took the exact same steps
+    // The auth tag could be appended or prepended to the encrypted data and it can be public (as far as I can tell)
+    // The only thing that it looks like you can't do is get the auth tag and append it to the file before it's written--there will have to be anothe way to transport it
+    // Potentially, this might require making a temp file with only the IV appended (or not, since I like having the IV first) and then when it writes to the final
+    // location, it reads the temp file and then adds the IV and tag to the file
+    const authTag = cipher.getAuthTag();
     // This emulates encrypting the key used for encrypting the file so that it can be sent to the intended party
     const rsaEncrypted = crypto.privateEncrypt({ key: privateKey, passphrase: sharedPassphrase }, tempKey);
     
     // This is where we actually "send" the file and start pulling the iv from it
     let iv;
-    const ivReadStream = fs.createReadStream('encrypted.gif', { end: 15 });
+    for await (const ivBytes of fs.createReadStream('encrypted.gif', { end: 15 })) {
+        iv = ivBytes;
+    }
 
-    ivReadStream.on('data', chunk => {
-        iv = chunk;
+    // When we have the IV from the file, we can go ahead and decrypt the key and then the actual file
+    const decryptedKey = crypto.publicDecrypt({ key: publicKey, passphrase: sharedPassphrase }, rsaEncrypted);
+
+    const decipher = crypto.createDecipheriv('aes-256-gcm', decryptedKey, iv);
+    // ensure GCM gets the auth tag it needs to verify the data hasn't been tampered with
+    decipher.setAuthTag(authTag);
+    
+    
+    // Start reading the encrypted file
+    const encryptedReadStream = fs.createReadStream('encrypted.gif', { start: 16 });
+    const resultWriteStream = fs.createWriteStream('result.gif');
+    
+    // Do something when the result file has been written
+    encryptedReadStream.on('close', () => {
+        console.log('Result file written successfully');
+    });
+    
+    // Handle it if it fails
+    encryptedReadStream.on('error', (err) => {
+        if (err) console.error(err);
     });
 
-    ivReadStream.on('close', () => {
-        // When we have the IV from the file, we can go ahead and decrypt the key and then the actual file
-        const decryptedKey = crypto.publicDecrypt({ key: publicKey, passphrase: sharedPassphrase }, rsaEncrypted);
-
-        const decipher = crypto.createDecipheriv('aes-256-cbc', decryptedKey, iv);
-        
-        
-        // Start reading the encrypted file
-        const encryptedReadStream = fs.createReadStream('encrypted.gif', { start: 16 });
-        const resultWriteStream = fs.createWriteStream('result.gif');
-        
-        // Do something when the result file has been written
-        encryptedReadStream.on('close', () => {
-            console.log('Result file written successfully');
-        });
-        
-        // Handle it if it fails
-        encryptedReadStream.on('error', (err) => {
-            if (err) console.error(err);
-        });
-
-        // Let the decryption begin!
-        encryptedReadStream
-            .pipe(decipher)
-            .pipe(resultWriteStream);
-    });
+    // Let the decryption begin!
+    encryptedReadStream
+        .pipe(decipher)
+        .pipe(resultWriteStream);
 });
 
 // randomly create the IV, key, and cipher for this demo
 // In a real application, the key would likely not be generated each time before saving the file
 const tempKeyIv = crypto.randomBytes(16);
 const tempKey = crypto.randomBytes(32);
-const cipher = crypto.createCipheriv('aes-256-cbc', tempKey, tempKeyIv);
+const cipher = crypto.createCipheriv('aes-256-gcm', tempKey, tempKeyIv);
 
 readStream
     .pipe(cipher)
